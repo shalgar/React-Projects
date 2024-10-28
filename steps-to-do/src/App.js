42b19bd08f9d0a27076bb08dbf23ef7c7337d80c
shalgar,rohit rajkumar <rshalgar@itron.com>
2024-10-28 17:51:18 -0600

    Stateful steps

---
+++ steps-to-do/src/App.js
@@ -0,0 +1,44 @@
+import { useState } from "react";
+
+ const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
+
+function App() {
+  const [step, setStep] = useState(1);
+  const [isOpen, setIsOpen] = useState(true);
+
+  function handlePrevious(){
+    if(step > 1) setStep(step - 1);
+  }
+
+  function handleNext(){
+    if(step < 3) setStep(step + 1);
+  }
+
+  return (
+    <>
+
+    <button className="close" onClick={()=> setIsOpen(!isOpen)}>&times;</button>
+    { isOpen &&
+      <div className="steps">
+      <div className="numbers">
+        <div className={ step >= 1 ?"active" :""}>1</div>
+        <div className={ step >= 2 ?"active" :""}>2</div>
+        <div className={ step >= 3 ?"active" :""}>3</div>
+      </div>
+        <p className="messages"> Step {step}: {messages[step -1]}</p>
+        <div className="buttons">
+        <button style={{backgroundColor:'purple', color:'white'}} onClick={handlePrevious}>prev</button>
+        <button style={{backgroundColor:'purple', color:'white'}} onClick={handleNext}>next</button>
+      </div>
+    </div>
+    }
+    
+    </>
+  );
+}
+
+export default App;
