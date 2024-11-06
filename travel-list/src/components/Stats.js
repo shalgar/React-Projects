6f4d869535013d3bcbc91656a4e39c20d4fea2d8
shalgar,rohit rajkumar <rshalgar@itron.com>
2024-11-05 21:30:49 -0600

    Initial commits

--- travel-list/src/components/Stats.js
+++ travel-list/src/components/Stats.js
@@ -1,16 +1,23 @@
-export default function Stats({items}) {
-    if(!items.length) {
-      return <em>Start Adding some Items 🚀</em>
+export default function Stats({ items }) {
+    if (!items.length) {
+      return <em>Start adding some items 🚀</em>;
     }
+    
     const numItems = items.length;
     const packedItems = items.filter(item => item.packed).length;
-    const packedPerc = Math.round(packedItems/numItems) * 100;
+    const packedPerc = Math.round((packedItems / numItems) * 100);
+  
     return (
       <footer className="stats">
-         { packedPerc === 100 ? 
-              <em>All the items are packed, You are all set!</em> :
-              <em>You Have {numItems} on your list, and you already Packed {packedItems} items ({packedPerc}%)</em>
-          }
+        {packedPerc === 100 ? (
+          <em>All the items are packed. You are all set!</em>
+        ) : packedPerc === 0 ? (
+          <em>Start packing some items...</em>
+        ) : (
+          <em>
+            You have {numItems} items on your list, and you've already packed {packedItems} items ({packedPerc}%)
+          </em>
+        )}
       </footer>
-    )
-  }
+    );
+}
